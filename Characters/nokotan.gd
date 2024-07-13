c20f4e466e1ee5a93c926cfb8dc58b80a62313aa
pontakornth <pontakorn61@gmail.com>
2024-07-13 15:12:39 +0700

    Add game over

--- Characters/nokotan.gd
+++ Characters/nokotan.gd
@@ -14,3 +14,9 @@ func _physics_process(delta):
 	if koshitan != null:
 		velocity = position.direction_to(koshitan.position) * speed
 	move_and_slide()
+
+
+func _on_area_2d_body_entered(body):
+	# Get game over if it is Koshitan
+	if body is Koshitan:
+		get_tree().change_scene_to_file("res://System/game_over.tscn")
